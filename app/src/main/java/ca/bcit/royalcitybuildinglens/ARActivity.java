5c1aeba7139dc35deb93a2e07fa2208c91a259bf
EmilyDeLisle <emily.delisle@gmail.com>
2020-02-10 15:06:51 -0800

    Initial commit

---
+++ app/src/main/java/ca/bcit/royalcitybuildinglens/ARActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.royalcitybuildinglens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
