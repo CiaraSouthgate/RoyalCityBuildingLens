deb65935f37bba67700683728f9669e64fce536f
EmilyDeLisle <emily.delisle@gmail.com>
2020-03-01 15:41:42 -0800

    Add current location detection and movement

--- app/src/main/java/ca/bcit/royalcitybuildinglens/MapsActivity.java
+++ app/src/main/java/ca/bcit/royalcitybuildinglens/MapsActivity.java
@@ -1,8 +1,18 @@
 package ca.bcit.royalcitybuildinglens;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,23 +20,58 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Adds a circle on the map at the given location
+     * @param location Location
+     */
+    private void addLocationToMap(Location location) {
+
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        mMap.clear();
+        mMap.addCircle(new CircleOptions()
+                .center(latlng)
+                .radius(5)
+                .strokeColor(Color.WHITE)
+                .fillColor(Color.BLUE));
+
+        int zoomLevel = 18;
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,zoomLevel));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                addLocationToMap(lastKnownLocation);
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -41,13 +86,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng army_and_navy = new LatLng(49.2036415, -122.9064928);
-        mMap.addMarker(new MarkerOptions().position(army_and_navy).title("502 Columbia St"));
-        mMap.animateCamera( CameraUpdateFactory.zoomTo( 4f ) );
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(army_and_navy));
+        // Disable scrolling & rotating
+        UiSettings settings = mMap.getUiSettings();
+        settings.setScrollGesturesEnabled(false);
+        settings.setRotateGesturesEnabled(false);
+
+        // Zoom into users location
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                addLocationToMap(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+            @Override
+            public void onProviderEnabled(String s) {}
+
+            @Override
+            public void onProviderDisabled(String s) {}
+        };
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                    0, locationListener);
+            Location lastKnownLocation = locationManager
+                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            addLocationToMap(lastKnownLocation);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
     }
 
+    /**
+     * Open AR activity
+     * @param view view
+     */
     public void onClickAR(View view) {
         Intent intent = new Intent(this, ARActivity.class);
         startActivity(intent);
