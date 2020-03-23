c65b0c0a44952a81d18ce185c62b3033a5bad326
Louis Lu <j95159@gmail.com>
2020-03-23 14:37:10 -0700

    modified from master, implemented AR view but haven't tested it out.

--- app/src/main/java/ca/bcit/royalcitybuildinglens/ARActivity.java
+++ app/src/main/java/ca/bcit/royalcitybuildinglens/ARActivity.java
@@ -2,13 +2,155 @@ package ca.bcit.royalcitybuildinglens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Plane;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import uk.co.appoly.arcorelocation.LocationMarker;
+import uk.co.appoly.arcorelocation.LocationScene;
+import uk.co.appoly.arcorelocation.rendering.LocationNode;
+import uk.co.appoly.arcorelocation.rendering.LocationNodeRender;
+import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper;
 
 public class ARActivity extends AppCompatActivity {
+    private boolean hasFinishedLoading = false;
+    private ArSceneView arSceneView;
+    private ViewRenderable buildingInfoLayoutRenderable;
+    private LocationScene locationScene;
+    private Snackbar loadingMessageSnackbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
+        arSceneView = findViewById(R.id.ar_scene_view);
+        CompletableFuture<ViewRenderable> exampleLayout =
+                ViewRenderable.builder()
+                        .setView(this, R.layout.building_info_layout)
+                        .build();
+
+        CompletableFuture.allOf(
+                exampleLayout)
+                .handle(
+                        (notUsed, throwable) -> {
+                            // When you build a Renderable, Sceneform loads its resources in the background while
+                            // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                            // before calling get().
+
+                            if (throwable != null) {
+                                Toast.makeText(
+                                        this, "Unable to load renderables.", Toast.LENGTH_LONG)
+                                        .show();
+                                return null;
+                            }
+
+                            try {
+                                buildingInfoLayoutRenderable = exampleLayout.get();
+                                hasFinishedLoading = true;
+
+                            } catch (InterruptedException | ExecutionException ex) {
+                                Toast.makeText(
+                                        this, "Unable to load renderables.", Toast.LENGTH_LONG)
+                                        .show();
+                            }
+
+                            return null;
+                        });
+        arSceneView
+                .getScene()
+                .addOnUpdateListener(
+                        frameTime -> {
+                            if (!hasFinishedLoading) {
+                                return;
+                            }
+
+                            if (locationScene == null) {
+                                // If our locationScene object hasn't been setup yet, this is a good time to do it
+                                // We know that here, the AR components have been initiated.
+                                locationScene = new LocationScene(this, this, arSceneView);
+
+                                // Now lets create our location markers.
+                                // First, a layout
+                                LocationMarker layoutLocationMarker = new LocationMarker(
+                                        -122.906519,
+                                        49.203622,
+                                        getBuildingView()
+                                );
+
+                                // An example "onRender" event, called every frame
+                                // Updates the layout with the markers distance
+                                layoutLocationMarker.setRenderEvent(new LocationNodeRender() {
+                                    @Override
+                                    public void render(LocationNode node) {
+                                        View eView = buildingInfoLayoutRenderable.getView();
+                                        TextView distanceTextView = eView.findViewById(R.id.building_one_tv);
+                                        //distanceTextView.setText(node.getDistance() + "M");
+                                    }
+                                });
+                                // Adding the marker
+                                locationScene.mLocationMarkers.add(layoutLocationMarker);
+
+                            }
+
+                            Frame frame = arSceneView.getArFrame();
+                            if (frame == null) {
+                                return;
+                            }
+
+                            if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+                                return;
+                            }
+
+                            if (locationScene != null) {
+                                locationScene.processFrame(frame);
+                            }
+
+                            if (loadingMessageSnackbar != null) {
+                                for (Plane plane : frame.getUpdatedTrackables(Plane.class)) {
+                                    if (plane.getTrackingState() == TrackingState.TRACKING) {
+                                        hideLoadingMessage();
+                                    }
+                                }
+                            }
+                        });
+
+        ARLocationPermissionHelper.requestPermission(this);
     }
+    private Node getBuildingView() {
+        Node base = new Node();
+        base.setRenderable(buildingInfoLayoutRenderable);
+        Context c = this;
+        // Add  listeners etc here
+        View eView = buildingInfoLayoutRenderable.getView();
+        eView.setOnTouchListener((v, event) -> {
+            Toast.makeText(
+                    c, "Location marker touched.", Toast.LENGTH_LONG)
+                    .show();
+            return false;
+        });
+
+        return base;
+    }
+    private void hideLoadingMessage() {
+        if (loadingMessageSnackbar == null) {
+            return;
+        }
+
+        loadingMessageSnackbar.dismiss();
+        loadingMessageSnackbar = null;
+    }
+
 }
