0599fc8c8967d1c752f76ff1e36966cc2b516f2c
Louis Lu <j95159@gmail.com>
2020-03-26 13:22:53 -0700

    Ar activity able to launch!!!! version 0

--- app/src/main/java/ca/bcit/royalcitybuildinglens/ARActivity.java
+++ app/src/main/java/ca/bcit/royalcitybuildinglens/ARActivity.java
@@ -1,16 +1,30 @@
 package ca.bcit.royalcitybuildinglens;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.rendering.ViewRenderable;
@@ -22,19 +36,26 @@ import uk.co.appoly.arcorelocation.LocationMarker;
 import uk.co.appoly.arcorelocation.LocationScene;
 import uk.co.appoly.arcorelocation.rendering.LocationNode;
 import uk.co.appoly.arcorelocation.rendering.LocationNodeRender;
+import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper;
 
 
 public class ARActivity extends AppCompatActivity {
+    private static final String TAG = "ARActivity";
     private ArSceneView arSceneView;
     private ViewRenderable buildingRenderable;
     private LocationScene locationScene;
     private boolean hasFinishedLoading = false;
+    private Snackbar loadingMessageSnackbar = null;
+    private boolean installRequested;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
+        arSceneView = findViewById(R.id.ar_scene_view);
+
+        // Build building information renderable from building_info_Layout.xml
         CompletableFuture<ViewRenderable> buildingLayout =
                 ViewRenderable.builder()
                 .setView(this, R.layout.building_info_layout)
@@ -44,13 +65,13 @@ public class ARActivity extends AppCompatActivity {
                 buildingLayout)
                 .handle(
                         (notUsed, throwable) -> {
-                            // When you build a Renderable, Sceneform loads its resources in the background while
-                            // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
-                            // before calling get().
+                            // When Renderable is builded, Sceneform loads its resources
+                            // in the background while returning a CompletableFuture.
+                            // Call handle(), thenAccept(), or check isDone() before calling get().
 
                             if (throwable != null) {
                                 Toast.makeText(
-                                        this, "Unable to load renderables.", Toast.LENGTH_LONG)
+                                        this, "Unable to load renderables. "+ throwable.getMessage(), Toast.LENGTH_LONG)
                                         .show();
                                 return null;
                             }
@@ -61,7 +82,7 @@ public class ARActivity extends AppCompatActivity {
 
                             } catch (InterruptedException | ExecutionException ex) {
                                 Toast.makeText(
-                                        this, "Unable to load renderables.", Toast.LENGTH_LONG)
+                                        this, "Unable to load renderables. "+ ex.getMessage(), Toast.LENGTH_LONG)
                                         .show();
                             }
 
@@ -77,15 +98,13 @@ public class ARActivity extends AppCompatActivity {
                             }
 
                             if (locationScene == null) {
-                                // If our locationScene object hasn't been setup yet, this is a good time to do it
-                                // We know that here, the AR components have been initiated.
+
                                 locationScene = new LocationScene(this, this, arSceneView);
 
-                                // Now lets create our location markers.
-                                // First, a layout
+                                // Building building information with layout( getBuildingView() )
                                 LocationMarker layoutLocationMarker = new LocationMarker(
-                                        -4.849509,
-                                        42.814603,
+                                        -122.906509,
+                                        49.203645,
                                         getBuildingView()
                                 );
 
@@ -134,12 +153,161 @@ public class ARActivity extends AppCompatActivity {
         View eView = buildingRenderable.getView();
         eView.setOnTouchListener((v, event) -> {
             Toast.makeText(
-                    c, "Location marker touched.", Toast.LENGTH_LONG)
+                    c, "Touched.", Toast.LENGTH_LONG)
                     .show();
             return false;
         });
 
         return base;
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        if (locationScene != null) {
+            locationScene.resume();
+        }
+
+        if (arSceneView.getSession() == null) {
+            // If the session wasn't created yet, don't resume rendering.
+            // This can happen if ARCore needs to be updated or permissions are not granted yet.
+            try {
+                Session session = createArSession(this, installRequested);
+                if (session == null) {
+                    installRequested = ARLocationPermissionHelper.hasPermission(this);
+                    return;
+                } else {
+                    arSceneView.setupSession(session);
+                }
+            } catch (UnavailableException e) {
+                handleSessionException(this, e);
+            }
+        }
+
+        try {
+            arSceneView.resume();
+        } catch (CameraNotAvailableException ex) {
+            Toast.makeText(
+                    this, "Unable to get camera", Toast.LENGTH_LONG)
+                    .show();
+            finish();
+            return;
+        }
+
+        if (arSceneView.getSession() != null) {
+            showLoadingMessage();
+        }
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (locationScene != null) {
+            locationScene.pause();
+        }
+
+        arSceneView.pause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        arSceneView.destroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] results) {
+        if (!ARLocationPermissionHelper.hasPermission(this)) {
+            if (!ARLocationPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                ARLocationPermissionHelper.launchPermissionSettings(this);
+            } else {
+                Toast.makeText(
+                        this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                        .show();
+            }
+            finish();
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Standard Android full-screen functionality.
+            getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    private void showLoadingMessage() {
+        if (loadingMessageSnackbar != null && loadingMessageSnackbar.isShownOrQueued()) {
+            return;
+        }
+
+        loadingMessageSnackbar =
+                Snackbar.make(
+                        ARActivity.this.findViewById(android.R.id.content),
+                        R.string.plane_finding,
+                        Snackbar.LENGTH_INDEFINITE);
+        loadingMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+        loadingMessageSnackbar.show();
+    }
+
+    private void hideLoadingMessage() {
+        if (loadingMessageSnackbar == null) {
+            return;
+        }
+
+        loadingMessageSnackbar.dismiss();
+        loadingMessageSnackbar = null;
+    }
+    public static Session createArSession(Activity activity, boolean installRequested)
+            throws UnavailableException {
+        Session session = null;
+        // if we have the camera permission, create the session
+        if (ARLocationPermissionHelper.hasPermission(activity)) {
+            switch (ArCoreApk.getInstance().requestInstall(activity, !installRequested)) {
+                case INSTALL_REQUESTED:
+                    return null;
+                case INSTALLED:
+                    break;
+            }
+            session = new Session(activity);
+            // IMPORTANT!!!  ArSceneView needs to use the non-blocking update mode.
+            Config config = new Config(session);
+            config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+            session.configure(config);
+        }
+        return session;
+    }
+
+    public static void handleSessionException(
+            Activity activity, UnavailableException sessionException) {
+
+        String message;
+        if (sessionException instanceof UnavailableArcoreNotInstalledException) {
+            message = "Please install ARCore";
+        } else if (sessionException instanceof UnavailableApkTooOldException) {
+            message = "Please update ARCore";
+        } else if (sessionException instanceof UnavailableSdkTooOldException) {
+            message = "Please update this app";
+        } else if (sessionException instanceof UnavailableDeviceNotCompatibleException) {
+            message = "This device does not support AR";
+        } else {
+            message = "Failed to create AR session";
+            Log.e(TAG, "Exception: " + sessionException);
+        }
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+    }
 }
